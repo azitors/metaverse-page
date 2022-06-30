b578e9374be278951655d9e2a8b3313db63569a6
regonn <programmer@haizine.art>
2022-06-30 17:28:44 +0900

    rename Unity files

--- client/context/useAzitoUnity.ts
+++ client/context/useAzitoUnity.ts
@@ -2,10 +2,10 @@ import { useUnityContext } from 'react-unity-webgl';
 
 export const useAzitoUnity = () => {
   const { unityProvider, sendMessage } = useUnityContext({
-    loaderUrl: 'unity/build.loader.js',
-    dataUrl: 'unity/build.data',
-    frameworkUrl: 'unity/build.framework.js',
-    codeUrl: 'unity/build.wasm',
+    loaderUrl: 'unity/Unity.loader.js',
+    dataUrl: 'unity/Unity.data',
+    frameworkUrl: 'unity/Unity.framework.js',
+    codeUrl: 'unity/Unity.wasm',
   });
 
   return { unityProvider, sendMessage };
